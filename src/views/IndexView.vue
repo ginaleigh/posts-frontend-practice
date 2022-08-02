7b12f6e58406c26bb509078fd8c41eb631df97b8
Gina Mirando <gina.mirando5@gmail.com>
2022-08-01 20:15:55 -0500

    index.vue

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -19,3 +19,15 @@ export de4fault {
   },
 };
 </script>
+
+<template>
+  <div>
+    <h1>Blog Posts</h1>
+    <div v-for="post in posts" v-bind:key="post.id">
+      <h2>Title: {{ post.title }}</h2>
+      <img v-bind:src="post.image_url" v-bind:alt="post.title" />
+      <p>Chef: {{ post.chef }}</p>
+      <a v-bind:href="`/posts/${post.id}`">More info</a>
+    </div>
+  </div>
+</template>
